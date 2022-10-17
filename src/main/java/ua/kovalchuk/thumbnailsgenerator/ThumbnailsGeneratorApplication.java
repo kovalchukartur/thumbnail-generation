cc7cf27606cf357a3f09f12905e5d507f93f8fb9
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-10-17 17:16:20 +0300

    Init

---
+++ src/main/java/ua/kovalchuk/thumbnailsgenerator/ThumbnailsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.thumbnailsgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThumbnailsGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThumbnailsGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/kovalchuk/thumbnailsgenerator/ThumbnailsGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.thumbnailsgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThumbnailsGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
